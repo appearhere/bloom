c70dfb45a2d771fcadd2e32df9eeff3a38ff9807
Richard Palmer <richard.palmer@appearhere.co.uk>
2016-09-12 16:22:08 +0100

    WIP style guide colours

--- styleguide/Styleguide.js
+++ styleguide/Styleguide.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
+import Swatch from './components/Swatch/Swatch';
+
+const colours = [
+  'Black',
+  'Blue',
+  'Gold',
+  'Red',
+  'White',
+];
+
 export default () => (
-  <div>Bloom - Appear Here's pattern library & style guide</div>
+  <div>
+    <div>Bloom - Appear Here's pattern library & style guide</div>
+    {colours.map(c => <Swatch color={c} />)}
+  </div>
 );
