e470c3238a162aae48f98b79c90755f585e57835
Richard Palmer <richard.palmer@appearhere.co.uk>
2016-09-14 10:49:55 +0100

    fixup! WIP Icon component

--- components/stories/Icon.js
+++ components/stories/Icon.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import Icon from '../Icon/Icon';
 
-storiesOf('ScreenReadable', module)
+storiesOf('Icon', module)
   .add('default view', () => (
     <Icon name="example" />
   ));
