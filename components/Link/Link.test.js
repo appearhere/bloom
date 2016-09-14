6d22774d1e604000089731ac6537a008a9d65036
Richard Palmer <richard.palmer@appearhere.co.uk>
2016-09-14 14:40:46 +0100

    Add test to <Link /> component
    create-react-app now ships with Jest, so rather than going with tape as
    originally planned, we'll give Jest a shot.
    
    For more information:
    - http://facebook.github.io/jest/
    - https://github.com/substack/tape
    - https://github.com/rdjpalmer/create-react-app-tape

--- components/Link/Link.test.js
+++ components/Link/Link.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Styleguide from './Styleguide';
+import Link from './Link';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Styleguide />, div);
+  ReactDOM.render(<Link />, div);
 });
