001f605010f469dcd7b1505222284265d547fdd9
Richard Palmer <richard.palmer@appearhere.co.uk>
2016-09-21 15:23:03 +0100

    Add SectionHeader component
    SectionHeader gives you a title `<h1>` and strapline `<p>` in a
    consistent format. Work's great in `<section>` and <Hero />`
    elements

---
+++ components/SectionHeader/SectionHeader.js
@@ -0,0 +1,33 @@
+import React, { PropTypes } from 'react';
+import classnames from 'classnames';
+
+import m from '../../globals/modifiers.css';
+import css from './SectionHeader.css';
+
+const titleClasses = classnames(
+  css.base,
+  css.title,
+  m.fontTitleLarge,
+  m.uppercase,
+);
+
+const straplineClasses = classnames(
+  css.base,
+  css.strapline,
+  m.fontBase
+);
+
+const SectionHeader = ({ title, strapline, className, ...rest }) => (
+  <div className={ className } { ...rest }>
+    <h1 className={ titleClasses }>{ title }</h1>
+    <p className={ straplineClasses }>{ strapline }</p>
+  </div>
+);
+
+SectionHeader.propTypes = {
+  className: PropTypes.string,
+  title: PropTypes.node,
+  strapline: PropTypes.node,
+};
+
+export default SectionHeader;
