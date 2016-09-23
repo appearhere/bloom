02265fe704a04763c378262cf206fcb339c85552
Richard Palmer <richard.palmer@appearhere.co.uk>
2016-09-23 11:07:40 +0100

    Add Synopsis component, reorganise type components

---
+++ components/Type/Type.story.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { storiesOf, action } from '@kadira/storybook';
+import classnames from 'classnames';
+
+import SectionHeader from './SectionHeader/SectionHeader';
+import Synopsis from './Synopsis/Synopsis';
+import m from '../../globals/modifiers.css';
+
+storiesOf('Type', module)
+  .add('<SectionHeader />', () => (
+    <SectionHeader className={[m.pa48].join(' ')}
+      title="Bloom: Pattern library"
+      strapline="A styleguide & library of React components"
+    />
+  ))
+  .add('<SectionHeader /> centered', () => (
+    <SectionHeader className={[m.pa48, m.center].join(' ')}
+      title="Bloom: Pattern library"
+      strapline="A styleguide & library of React components"
+    />
+  ))
+  .add('<SectionHeader /> reverse', () => (
+    <SectionHeader className={[m.fgWhite, m.bgBlack, m.pa48].join(' ')}
+      title="Bloom: Pattern library"
+      strapline="A styleguide & library of React components"
+    />
+  )).add('<Synopsis /> default', () => (
+    <Synopsis className={[m.pa48].join(' ')}
+      title="Thousands of spaces, for any idea and budget."
+      body="Find a space that matches your price, location, and audience, or browse our destination guides for inspiration."
+    />
+  ))
+  .add('<Synopsis /> centered', () => (
+    <Synopsis className={[m.pa48, m.center].join(' ')}
+      title="Focus on your idea."
+      body="We’ve organised legals & deposits simply so you can focus on the one thing that really matters - making your idea happen."
+    />
+  ));
