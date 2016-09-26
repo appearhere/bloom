4487f00e97f3e9707054e8016067e826e3e66e8b
Richard Palmer <richard.palmer@appearhere.co.uk>
2016-09-26 12:32:51 +0100

    Add Swap animation component

--- components/Animate/Animate.story.js
+++ components/Animate/Animate.story.js
@@ -4,12 +4,12 @@ import { storiesOf } from '@kadira/storybook';
 import Sunrise from './Sunrise';
 import m from '../../globals/modifiers.css';
 
-const panels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+const sunrisePanels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
 storiesOf('Animation', module)
   .add('Sunrise', () => (
     <div>
-      { panels.map(() => (
+      { sunrisePanels.map(() => (
         <Sunrise percent={ 50 }>
           <div
             className={ m.bgPrimary }
@@ -24,3 +24,7 @@ storiesOf('Animation', module)
       )) }
     </div>
   ));
+  // The swap story is going to link straight to
+  // the GridFader as thats a much better example
+  // of it in use
+  // .add('Swap', () => ());
