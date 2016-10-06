fe630a48ce64f189f043210c97ca11fc66d250e0
Richard Palmer <richard.palmer@appearhere.co.uk>
2016-10-06 09:28:36 +0100

    Make `<Wrapper />` component accept styling from the outside and update margins
    1. `<Wrapper />` now accepts a className prop and consumes it along side its own
    default styles
    2. `<Wrapper />` now has increased margins at larger sizes

--- components/Wrapper/Wrapper.js
+++ components/Wrapper/Wrapper.js
@@ -1,7 +1,15 @@
 import React, { PropTypes } from 'react';
+import cx from 'classnames';
 
 import css from './Wrapper.css';
 
-export default ({ children }) => (
-  <div className={ css.root }>{ children }</div>
+const Wrapper = ({ children, className }) => (
+  <div className={ cx(css.root, className) }>{ children }</div>
 );
+
+Wrapper.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+};
+
+export default Wrapper;
