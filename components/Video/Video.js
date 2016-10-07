41d32f6049b3ab8f94f82581b2522f397ab563d4
Richard Palmer <richard.palmer@appearhere.co.uk>
2016-10-07 13:00:56 +0100

    Add aspectRatio support to video component

--- components/Video/Video.js
+++ components/Video/Video.js
@@ -8,12 +8,16 @@ import PlayBtn from './PlayBtn/PlayBtn';
 import css from './Video.css';
 
 const Video = (props) => {
-  const { children: source, className, controls, ...rest } = props;
+  const { children: source, className, controls, aspectRatio, ...rest } = props;
   const classes = cx(css.root, className);
 
+  // react-html5video needs to support style props for this to work
   return (
     <ReactVideo
       className={ classes }
+      style={ {
+        paddingBottom: `${height}%`,
+      } }
       controls={ controls }
       { ...rest }
     >
@@ -33,10 +37,20 @@ Video.propTypes = {
   ]).isRequired,
   className: PropTypes.string,
   controls: PropTypes.bool,
+  aspectRatio: (props, propName, componentName) => {
+    if (props[propName].indexOf(':') < 0) {
+      return new Error(
+        'Invalid prop `' + propName + '` supplied to' +
+        ' `' + componentName + '`. Invalid format. Aspect ratio should be ' +
+        'supplied like 4:3.'
+      );
+    }
+  }
 };
 
 Video.defaultProps = {
   controls: false,
+  aspectRatio: '16:9',
 };
 
 export default Video;
