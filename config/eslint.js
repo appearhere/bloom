70b45fbf592665453e1eb4cd85b4f9d9ad222a9f
Richard Palmer <richard.palmer@appearhere.co.uk>
2016-10-24 15:24:18 +0100

    Disable problematic eslint import rules

--- config/eslint.js
+++ config/eslint.js
@@ -1,7 +1,18 @@
 module.exports = {
   extends: [
     'airbnb',
-    '@appearhere/appearhere'
+    '@appearhere/appearhere',
   ],
   parser: 'babel-eslint',
+  rules: {
+    // AirBnb config includes the imports plugin, so we actually have to
+    // disable the rules manually
+    'import/no-unresolved': 0,
+
+    // We use a webpack alias to make using our components in the style guide a
+    // nicer. import/no-extraneous-dependencies do NOT like that one bit.
+    // We'll disable it until theres a decent work around
+    'import/no-extraneous-dependencies': 0,
+    'import/extensions': 0,
+  }
 };
