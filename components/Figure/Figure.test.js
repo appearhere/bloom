628c38b479f10453bbdfcc4c361a737ba800a469
Simon Lau <superslau@gmail.com>
2016-10-31 10:46:26 +0000

    Add `Figure` component
    Component for semantically marking up diagrams, illustrations, photos and code examples.

---
+++ components/Figure/Figure.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Figure from './Figure';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <Figure caption="">
+      Yes
+    </Figure>,
+    div
+  );
+});
