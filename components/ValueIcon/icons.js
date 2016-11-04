fba0f9393dbde0d833409728b033ea4ff3822967
Simon Lau <superslau@gmail.com>
2016-11-04 15:13:30 +0000

    fixup! fixup! Add `<ValueIcon>`

--- components/ValueIcon/icons.js
+++ components/ValueIcon/icons.js
@@ -1,13 +1,21 @@
 import svgClean from '../../utils/svg-clean';
 import svgReplaceClassName from '../../utils/svg-replaceClassname';
 import css from './ValueIcon.css';
-/* eslint-disable global-require */
+
+import iconPunch from './icons/icon-punch.svg';
+import iconThumbsUp from './icons/icon-thumbs-up.svg';
+import iconNoBull from './icons/icon-no-bull.svg';
+import iconHandshake from './icons/icon-handshake.svg';
+import iconOpen from './icons/icon-open.svg';
+import iconScissors from './icons/icon-scissors.svg';
+
+const clean = icon => svgReplaceClassName(svgClean(icon), css);
+
 export default {
-  'boxing-glove': svgReplaceClassName(svgClean(require('./icons/icon-punch.svg')), css),
-  'thumbs-up': svgReplaceClassName(svgClean(require('./icons/icon-thumbs-up.svg')), css),
-  'no-bull': svgReplaceClassName(svgClean(require('./icons/icon-no-bull.svg')), css),
-  handshake: svgReplaceClassName(svgClean(require('./icons/icon-handshake.svg')), css),
-  open: svgReplaceClassName(svgClean(require('./icons/icon-open.svg')), css),
-  scissors: svgReplaceClassName(svgClean(require('./icons/icon-scissors.svg')), css),
+  'boxing-glove': clean(iconPunch),
+  'thumbs-up': clean(iconThumbsUp),
+  'no-bull': clean(iconNoBull),
+  handshake: clean(iconHandshake),
+  open: clean(iconOpen),
+  scissors: clean(iconScissors),
 };
-/* eslint-ensable global-require */
