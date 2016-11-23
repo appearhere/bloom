56cc8cd1648311a98301abbce687ec5998e5f0eb
Richard Palmer <richard.palmer@appearhere.co.uk>
2016-11-23 12:20:57 +0000

    Add video support to `<DeviceFrame />` components

--- components/DeviceFrame/DeviceFrame.story.js
+++ components/DeviceFrame/DeviceFrame.story.js
@@ -3,6 +3,8 @@ import { storiesOf } from '@kadira/storybook';
 import Macbook from './Macbook';
 import IPhone from './IPhone';
 
+import iphoneVideo from './nike-screen.mp4';
+
 const macbookImages = [{
   src: 'https://source.unsplash.com/random/1440x900',
   alt: '',
@@ -19,6 +21,7 @@ const iphoneImages = [{
   alt: '',
 }];
 
+
 storiesOf('Device frame', module)
   .add('Macbook', () => (
     <Macbook>
@@ -30,8 +33,24 @@ storiesOf('Device frame', module)
       { macbookImages }
     </Macbook>
   ))
+  .add('Macbook Video', () => (
+    <Macbook video loop>
+      <source
+        src="https://s3-eu-west-1.amazonaws.com/appearhere/assets/bloom/example-video.mp4"
+        type="video/mp4"
+      />
+    </Macbook>
+  ))
   .add('IPhone', () => (
     <IPhone>
       { iphoneImages }
+    </IPhone>
+  ))
+  .add('IPhone Video', () => (
+    <IPhone video loop>
+      <source
+        src={ iphoneVideo }
+        type="video/mp4"
+      />
     </IPhone>
   ));
