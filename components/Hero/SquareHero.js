d89fd64ac55e7e12f364abe078a4df5fb4e73101
Richard Palmer <richard.palmer@appearhere.co.uk>
2016-11-30 12:20:30 +0000

    Add VideoWithRichPoster support to SquareHero

--- components/Hero/SquareHero.js
+++ components/Hero/SquareHero.js
@@ -1,12 +1,15 @@
 import React, { PropTypes, createElement } from 'react';
 import cx from 'classnames';
 
+import VideoWithRichPoster from '../VideoWithRichPoster/VideoWithRichPoster';
+
 import defaultCss from './SquareHero.css';
 
 const SquareHero = (props) => {
   const {
     children,
     image,
+    videoProps,
     alt,
     css,
     className,
@@ -21,7 +24,10 @@ const SquareHero = (props) => {
       className={ cx(defaultCss.root, defaultCss[headingSide], className) }
     >
       <div className={ cx(defaultCss.imageContainer, css.image) }>
-        <img className={ defaultCss.image } src={ image } alt={ alt } />
+        { videoProps
+          ? <VideoWithRichPoster { ...videoProps } className={ cx(defaultCss.video, css.video) } />
+          : <img className={ defaultCss.image } src={ image } alt={ alt } />
+        }
       </div>
       <div className={ cx(defaultCss.square, css.square) }>
         { createElement(
@@ -36,15 +42,22 @@ const SquareHero = (props) => {
 
 SquareHero.propTypes = {
   children: PropTypes.node.isRequired,
-  image: PropTypes.string.isRequired,
+  image: PropTypes.string,
   alt: PropTypes.string.isRequired,
   className: PropTypes.string,
   css: PropTypes.shape({
     square: PropTypes.string,
     image: PropTypes.string,
+    video: PropTypes.string,
   }),
   level: PropTypes.number,
   headingSide: PropTypes.oneOf(['left', 'right']),
+  videoProps: PropTypes.shape({
+    videoClassName: PropTypes.string,
+    posterClassName: PropTypes.string,
+    posterSrc: PropTypes.node.isRequired,
+    videoSrc: PropTypes.node.isRequired,
+  }),
 };
 
 SquareHero.defaultProps = {
