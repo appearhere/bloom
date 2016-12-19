1367c2990e060ac26194bc66d133bf31ea610f8a
Richard Palmer <richard.palmer@appearhere.co.uk>
2016-12-19 16:10:02 +0000

    Add `<Banner />` component
    Banner component for displaying important information in its own container.
    Likely to be used for notifications, or other content which requires should
    take the users attention away from the main content. Can be dismissable by
    supplying an `onClose` function

---
+++ components/Banner/Banner.js
@@ -0,0 +1,48 @@
+import React, { PropTypes } from 'react';
+import cx from 'classnames';
+
+import BtnContainer from '../BtnContainer/BtnContainer';
+import Icon from '../Icon/Icon';
+import css from './Banner.css';
+
+const contexts = {
+  ERROR: 'error',
+  SUCCESS: 'success',
+};
+
+const Banner = ({ children, className, context, onClose, ...rest }) => (
+  <div
+    { ...rest }
+    className={ cx(
+      css.root,
+      css[context],
+      onClose ? css.dismissable : null,
+      className,
+    ) }
+  >
+    <div
+      className={ cx(
+        css.inner,
+      ) }
+    >
+      { children }
+    </div>
+    { onClose && (
+      <BtnContainer className={ css.dismissContainer } onClick={ onClose }>
+        <Icon name="cross" />
+      </BtnContainer>
+    ) }
+  </div>
+);
+
+Banner.propTypes = {
+  className: PropTypes.string,
+  context: PropTypes.oneOf([
+    contexts.ERROR,
+    contexts.SUCCESS,
+  ]),
+  children: PropTypes.node,
+  onClose: PropTypes.func,
+};
+
+export default Banner;
---
+++ components/Banner/Banner.story.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { storiesOf, action } from '@kadira/storybook';
+import Banner from './Banner';
+
+storiesOf('Banner', module)
+  .add('Default', () => (
+    <Banner>
+      Important information that you should look at right now
+    </Banner>
+  ))
+  .add('Success', () => (
+    <Banner context="success">
+      You did something right! 👏
+    </Banner>
+  ))
+  .add('Error', () => (
+    <Banner context="error">
+      Something went wrong 😕
+    </Banner>
+  ))
+  .add('Dismissable', () => (
+    <Banner onClose={ action('Closing banner') }>
+      Important information that you should look at right now
+    </Banner>
+  ));
