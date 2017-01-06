dd0683f57516d3d455728ccc9aea356361db587e
Richard Palmer <richard.palmer@appearhere.co.uk>
2017-01-06 12:17:58 +0000

    Refactor Card/LinkedCard/SpaceCard
    - Merges `<Card />` & `<LinkedCard />` to remove duplication of hover styles an make easier to reuse
    - Introduces `<PictureCard />` for placing card contents over an image
    - Refactors `<SpaceCard />` to use `<PictureCard />` as its base. Renamed to `<SpaceFeatureCard />` to make room for the inevitable `<SpaceListingCard />`.

---
+++ components/Cards/Card/Card.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import Card from './Card';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  render(<Card />, div);
+});
+
+it('renders a div by default', () => {
+  const div = document.createElement('div');
+  let component;
+
+  render(
+    <div ref={ (c) => { component = c; } }>
+      <Card />
+    </div>,
+    div
+  );
+
+  expect(component.children[0].tagName).toBe(div.tagName);
+});
+
+it('renders an anchor when provided a href', () => {
+  const div = document.createElement('div');
+  const anchor = document.createElement('a');
+  let component;
+
+  render(
+    <div ref={ (c) => { component = c; } }>
+      <Card href="#" />
+    </div>,
+    div
+  );
+
+  expect(component.children[0].tagName).toBe(anchor.tagName);
+});
--- components/Cards/Cards.story.js
+++ components/Cards/Cards.story.js
@@ -1,37 +1,99 @@
 import React from 'react';
-import { storiesOf } from '@kadira/storybook';
+import { storiesOf, action } from '@kadira/storybook';
 
-import Card from './Card';
-import LinkedCard from './LinkedCard';
-import SpaceCard from './SpaceCard';
+import Card from './Card/Card';
+import PictureCard from './PictureCard/PictureCard';
+import SpaceFeatureCard from './SpaceFeatureCard/SpaceFeatureCard';
+import EditorialCard from './EditorialCard/EditorialCard';
+import GuideCard from './EditorialCard/GuideCard';
+import EventCard from './EditorialCard/EventCard';
 
-storiesOf('Card', module)
-  .add('Plain', () => (
-    <Card image="https://source.unsplash.com/random/536x800">
-      Hello, world
-    </Card>
+storiesOf('Cards', module)
+  .add('Default', () => (
+    <Card>Hello, world</Card>
   ))
-  .add('with Link', () => (
-    <LinkedCard href="#" image="https://source.unsplash.com/random/536x800">
-      Hello, world
-    </LinkedCard>
+  .add('Default with link', () => (
+    <Card href="#">Hello, world</Card>
   ))
-  .add('SpaceCard', () => (
-    <SpaceCard
-      name="Rue De Chazelles"
-      image="https://source.unsplash.com/random/600x600"
-      price="£46 / day"
-      location="London"
+  .add('PictureCard', () => (
+    <PictureCard
+      style={ { minHeight: '10vh' } }
+      src="https://source.unsplash.com/random"
+    >
+      Hello, world
+    </PictureCard>
+  ))
+  .add('PictureCard with link', () => (
+    <PictureCard
+      style={ { minHeight: '10vh' } }
       href="#"
+      src="https://source.unsplash.com/random"
+    >
+      Hello, world
+    </PictureCard>
+  ))
+  .add('PictureCard with centered content', () => (
+    <PictureCard
+      style={ { minHeight: '10vh' } }
+      src="https://source.unsplash.com/random"
+      center
+    >
+      Hello, world
+    </PictureCard>
+  ))
+  .add('SpaceFeatureCard with link', () => (
+    <SpaceFeatureCard
+      name="Mulberry Street"
+      price="$1500 / day"
+      location="New York"
+      href="#"
+      src="https://source.unsplash.com/random"
     />
   ))
-  .add('SpaceCard: square', () => (
-    <SpaceCard
-      name="Rue De Chazelles"
-      image="https://source.unsplash.com/random/600x600"
-      price="£46 / day"
-      location="London"
+  .add('EditorialCard', () => (
+    <EditorialCard
+      title="Mulberry Street"
       href="#"
-      square
+      src="https://source.unsplash.com/random/500x500"
+    >
+      Nam erat urna, tincidunt eleifend libero ut, posuere dictum augue.
+    </EditorialCard>
+  ))
+  .add('GuideCard locked', () => (
+    <GuideCard
+      title="Mulberry Street"
+      href="#"
+      src="https://source.unsplash.com/random/500x500"
+      description="Nam erat urna, tincidunt eleifend libero ut, posuere dictum augue."
+      unlockCallback={ (e) => {
+        e.stopPropagation();
+        action('Unlocking')(e);
+      } }
+    />
+  ))
+  .add('GuideCard unlocked', () => (
+    <GuideCard
+      title="Mulberry Street"
+      href="#"
+      src="https://source.unsplash.com/random/500x500"
+      description="Nam erat urna, tincidunt eleifend libero ut, posuere dictum augue."
+      downloadCallback={ (e) => {
+        e.stopPropagation();
+        action('Downloading')(e);
+      } }
+    />
+  ))
+  .add('EventCard', () => (
+    <EventCard
+      title="The Editors"
+      href="#"
+      src="https://source.unsplash.com/random/500x500"
+      description="Nam erat urna, tincidunt eleifend libero ut, posuere dictum augue."
+      location="London"
+      date="28th August 2016"
+      ctaCallback={ (e) => {
+        e.stopPropagation();
+        action('Redirecting')(e);
+      } }
     />
   ));
