cc65eee7877aab7a594bc13250dc16148ba10cfa
Richard Palmer <richard.palmer@appearhere.co.uk>
2017-02-08 17:12:36 +0000

    Add `<Badge />` component
    Provide additional context to other elements with badges. Works similar
    to our `<Btn />` component in that it accepts a context, and can be
    toggled to be hollow or not

---
+++ components/Badge/Badge.js
@@ -0,0 +1,27 @@
+import React, { PropTypes } from 'react';
+import cx from 'classnames';
+
+import css from './Badge.css';
+
+const Badge = ({ className, children, context, hollow, ...rest }) => (
+  <span
+    { ...rest }
+    className={ cx(
+      css.root,
+      css[context],
+      hollow ? css.hollow : null,
+      className
+    ) }
+  >
+    { children }
+  </span>
+);
+
+Badge.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.any,
+  context: PropTypes.oneOf(['primary', 'special']),
+  hollow: PropTypes.bool,
+};
+
+export default Badge;
---
+++ components/Badge/Badge.story.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+import Badge from './Badge';
+
+storiesOf('Badge', module)
+  .add('Default badge', () => (
+    <Badge>Soon</Badge>
+  ))
+  .add('Primary badge', () => (
+    <Badge context="primary">New</Badge>
+  ))
+  .add('Special badge', () => (
+    <Badge context="special">Verified</Badge>
+  ))
+  .add('Hollow badge', () => (
+    <Badge hollow>Unknown</Badge>
+  ))
+  .add('Primary hollow badge', () => (
+    <Badge context="primary" hollow>Place</Badge>
+  ))
+  .add('Special hollow badge', () => (
+    <Badge context="special" hollow>Collection</Badge>
+  ));
