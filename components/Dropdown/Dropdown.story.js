7a153d96f6c6fbc9f67ecafe1acd6817ff9d4040
Richard Palmer <richard.palmer@appearhere.co.uk>
2017-02-15 16:00:41 +0000

    Add `<Dropdown />` component
    Basic, stylised drop down, utilising `<Tether />` for positioning

---
+++ components/Dropdown/Dropdown.story.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+
+import Dropdown, { HORIZONTAL_ATTACHMENTS } from './Dropdown';
+import DropdownInner from './DropdownInner';
+
+const Target = () => <button>Anchor</button>;
+const DropdownContent = () => <div>Dropdown content, like a list of actions, helper text etc.</div>;
+
+storiesOf('DropdownInner', module)
+  .add('Default', () => (
+    <DropdownInner>
+      <DropdownContent />
+    </DropdownInner>
+  ));
+
+storiesOf('Dropdown', module)
+  .add('Default', () => (
+    <Dropdown
+      horizontalAttachment={ HORIZONTAL_ATTACHMENTS.RIGHT }
+      target={ <Target /> }
+      flushHorizontal
+      active
+    >
+      <DropdownContent />
+    </Dropdown>
+  ));
