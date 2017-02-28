264b2e529f01159f15a3fc6fbd5a3342b783746c
Richard Palmer <richard.palmer@appearhere.co.uk>
2017-02-28 16:02:39 +0000

    Add `<Blokk />` component
    Component for rendering a block in place of content. Useful for
    rendering empty states where text content is loaded in after the a
    component first renders

---
+++ components/Blokk/Blokk.js
@@ -0,0 +1,25 @@
+import React, { PropTypes } from 'react';
+import cx from 'classnames';
+
+import css from './Blokk.css';
+
+const Blokk = ({ variant, length, className }) => (
+  <span
+    className={ cx(css.root, css[variant], className) }
+    style={ {
+      maxWidth: `${length}em`,
+    } }
+  />
+);
+
+Blokk.propTypes = {
+  length: PropTypes.number,
+  variant: PropTypes.oneOf(['light', 'dark']),
+  className: PropTypes.string,
+};
+
+Blokk.defaultProps = {
+  variant: 'light',
+};
+
+export default Blokk;
