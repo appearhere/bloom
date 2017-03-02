d2c00071de83c0153d227a871cecc4d4a647f4e6
Richard Palmer <richard.palmer@appearhere.co.uk>
2017-03-02 14:45:29 +0000

    Add `<SiblingTransition />` component
    Component for animating between routes that live on the same level as
    each other

---
+++ components/SiblingTransition/SiblingTransition.js
@@ -0,0 +1,108 @@
+import React, { PropTypes, Component } from 'react';
+import { TransitionMotion, spring } from 'react-motion';
+
+import noop from '../../utils/noop';
+import css from './SiblingTransition.css';
+
+const DEFAULT_SPRING_CONFIG = { stiffness: 178, damping: 25 };
+
+export default class SiblingTransition extends Component {
+  static propTypes = {
+    springConfig: PropTypes.shape({
+      stiffness: PropTypes.number,
+      damping: PropTypes.number,
+    }),
+    animate: PropTypes.bool,
+    route: PropTypes.string.isRequired,
+    children: PropTypes.element,
+    shouldAnimateLeft: PropTypes.func,
+    didLeave: PropTypes.func,
+  };
+
+  static defaultProps = {
+    springConfig: DEFAULT_SPRING_CONFIG,
+    didLeave: noop,
+  };
+
+  componentWillReceiveProps(nextProps) {
+    const { route: oldRoute } = this.props;
+    const { route: newRoute, animate } = nextProps;
+
+    if (!animate) this.setState({ newRoute: null, oldRoute: null });
+    if (newRoute !== oldRoute) this.setState({ newRoute, oldRoute });
+  }
+
+  getStyles = () => {
+    const { springConfig } = this.props;
+
+    return {
+      x: spring(0, springConfig),
+    };
+  }
+
+  willEnter = () => {
+    const { shouldAnimateLeft, animate } = this.props;
+    const { newRoute, oldRoute } = this.state;
+
+    if (!animate) return { x: 0 };
+
+    const transitionLeft = shouldAnimateLeft(newRoute, oldRoute);
+
+    return {
+      x: transitionLeft ? 100 : -100,
+    };
+  }
+
+  willLeave = () => {
+    const { springConfig, animate, shouldAnimateLeft } = this.props;
+    const { newRoute, oldRoute } = this.state;
+
+    if (!animate) return { x: 0 };
+
+    const transitionLeft = shouldAnimateLeft(newRoute, oldRoute);
+
+    return {
+      x: spring(transitionLeft ? -100 : 100, springConfig),
+    };
+  }
+
+  render() {
+    const {
+      route,
+      children: child,
+      didLeave,
+      ...rest,
+    } = this.props;
+
+    return (
+      <TransitionMotion
+        { ...rest }
+        styles={ [{
+          key: route,
+          style: this.getStyles(),
+          data: child,
+        }] }
+        willEnter={ this.willEnter }
+        willLeave={ this.willLeave }
+        didLeave={ didLeave }
+      >
+        { interpolated => (
+          <div className={ css.container }>
+            { interpolated.map(({ key, data, style }) => (
+              <div
+                key={ key }
+                className={ css.animated }
+                style={ {
+                  position: style.x === 0 ? 'relative' : 'absolute',
+                  transform: `translate3d(${style.x}%, 0, 0)`,
+                } }
+              >
+                { data }
+              </div>
+            )) }
+          </div>
+        ) }
+      </TransitionMotion>
+    );
+  }
+}
