cfc325b181e6235ce0bfe1c6add93e1bbd39c734
Nick Maher <nick.maher@appearhere.co.uk>
2017-03-20 16:28:51 +0000

    Switch to @appearhere/mapbox-gl fork
    Enables us to find the markers in a cluster through this PR:
    https://github.com/appearhere/mapbox-gl-js/pull/2
    
    Awaiting a resolution to this issue
    https://github.com/mapbox/mapbox-gl-js/issues/3318

--- utils/mapboxgl/mapboxgl.js
+++ utils/mapboxgl/mapboxgl.js
@@ -1,6 +1,6 @@
 import { canUseDOM } from 'exenv';
 
-const mapboxgl = canUseDOM ? require('mapbox-gl/dist/mapbox-gl') : {};
+const mapboxgl = canUseDOM ? require('@appearhere/mapbox-gl/dist/mapbox-gl') : {};
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYXBwZWFyaGVyZSIsImEiOiJvUlJ0MWxNIn0.8_mzlmxdekKy99luyV4T7w';
 
