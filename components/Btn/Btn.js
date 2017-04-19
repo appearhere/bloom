6b8f9e94c695770660d1e9d5be740ee0acc0c251
Nick Maher <nick.maher@appearhere.co.uk>
2017-04-19 14:59:13 +0100

    Add blur and focus methods to `Btn`

--- components/Btn/Btn.js
+++ components/Btn/Btn.js
@@ -1,61 +1,70 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import classnames from 'classnames';
 import noop from '../../utils/noop';
 
 import css from './Btn.css';
 
-const Btn = (props) => {
-  const {
-    children,
-    className,
-    onClick,
-    type,
-    disabled,
-    context,
-    variant,
-    priority,
-    ...rest,
-  } = props;
+export default class Btn extends Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    className: PropTypes.string,
+    onClick: PropTypes.func,
+    type: PropTypes.oneOf(['submit', 'reset', 'button', 'menu']),
+    disabled: PropTypes.bool,
 
-  const classes = classnames(
-    css.root,
-    className,
-    css[context],
-    css[variant],
-    css[priority],
-  );
+    context: PropTypes.oneOf(['default', 'primary', 'danger', 'action', 'whiteout']),
+    variant: PropTypes.oneOf(['default', 'hollow', 'subtle']),
+    priority: PropTypes.oneOf(['high', 'normal']),
+  };
 
-  return (
-    <button
-      className={ classes }
-      type={ type }
-      onClick={ onClick }
-      disabled={ disabled }
-      { ...rest }
-    >
-      { children }
-    </button>
-  );
-};
+  static defaultProps = {
+    type: 'button',
+    onClick: noop,
+    context: 'default',
+    variant: 'default',
+    priority: 'normal',
+  };
 
-Btn.propTypes = {
-  children: PropTypes.node.isRequired,
-  className: PropTypes.string,
-  onClick: PropTypes.func,
-  type: PropTypes.oneOf(['submit', 'reset', 'button', 'menu']),
-  disabled: PropTypes.bool,
+  focus = () => {
+    this.button.focus();
+  };
 
-  context: PropTypes.oneOf(['default', 'primary', 'danger', 'action', 'whiteout']),
-  variant: PropTypes.oneOf(['default', 'hollow', 'subtle']),
-  priority: PropTypes.oneOf(['high', 'normal']),
-};
+  blur = () => {
+    this.button.blur();
+  }
 
-Btn.defaultProps = {
-  type: 'button',
-  onClick: noop,
-  context: 'default',
-  variant: 'default',
-  priority: 'normal',
-};
+  render() {
+    const {
+      children,
+      className,
+      onClick,
+      type,
+      disabled,
+      context,
+      variant,
+      priority,
+      ...rest,
+    } = this.props;
 
-export default Btn;
+    const classes = classnames(
+      css.root,
+      className,
+      css[context],
+      css[variant],
+      css[priority],
+    );
+
+    return (
+      <button
+        ref={ (c) => { this.button = c; } }
+        className={ classes }
+        type={ type }
+        onClick={ onClick }
+        disabled={ disabled }
+        { ...rest }
+      >
+        { children }
+      </button>
+    );
+  }
+}
