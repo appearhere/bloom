99bd90f41520ff51b73e1a39dfe6e02fbf3952f4
Faraz Khan <farazcsk@gmail.com>
2017-09-06 16:56:08 +0100

    Add metaMarkers functionality to MarkableMap

---
+++ components/Map/testMetaMarkers.js
@@ -0,0 +1,224 @@
+const metaMarkersA = {
+  id: '1',
+  name: 'Aesop',
+  label: 'A',
+  type: 'FeatureCollection',
+  features: [
+    {
+      geometry: {
+        coordinates: [
+          -0.3484224,
+          51.3960673,
+        ],
+        type: 'Point',
+      },
+      properties: {
+        category: 'Retail',
+        name: 'Aesop',
+      },
+      type: 'Feature',
+    },
+    {
+      geometry: {
+        coordinates: [
+          -0.349404970957125,
+          51.3949483990099,
+        ],
+        type: 'Point',
+      },
+      properties: {
+        category: 'Retail',
+        name: 'Aesop',
+      },
+      type: 'Feature',
+    },
+    {
+      geometry: {
+        coordinates: [
+          -0.348814,
+          51.398839,
+        ],
+        type: 'Point',
+      },
+      properties: {
+        category: 'Retail',
+        name: 'Aesop',
+      },
+      type: 'Feature',
+    },
+    {
+      geometry: {
+        coordinates: [
+          -0.348622,
+          51.399212,
+        ],
+        type: 'Point',
+      },
+      properties: {
+        category: 'Retail',
+        name: 'Aesop',
+      },
+      type: 'Feature',
+    },
+    {
+      geometry: {
+        coordinates: [
+          -0.348979,
+          51.399185,
+        ],
+        type: 'Point',
+      },
+      properties: {
+        category: 'Retail',
+        name: 'Aesop',
+      },
+      type: 'Feature',
+    },
+    {
+      geometry: {
+        coordinates: [
+          -0.351540131829349,
+          51.4072566688305,
+        ],
+        type: 'Point',
+      },
+      properties: {
+        category: 'Retail',
+        name: 'Aesop',
+      },
+      type: 'Feature',
+    },
+    {
+      geometry: {
+        coordinates: [
+          -0.34933990240097,
+          51.425163269043,
+        ],
+        type: 'Point',
+      },
+      properties: {
+        category: 'Retail',
+        name: 'Aesop',
+      },
+      type: 'Feature',
+    },
+    {
+      geometry: {
+        coordinates: [
+          -0.349287762648642,
+          51.4283193108185,
+        ],
+        type: 'Point',
+      },
+      properties: {
+        category: 'Retail',
+        name: 'Aesop',
+      },
+      type: 'Feature',
+    },
+    {
+      geometry: {
+        coordinates: [
+          -0.348165273894053,
+          51.4323932568167,
+        ],
+        type: 'Point',
+      },
+      properties: {
+        category: 'Retail',
+        name: 'Aesop',
+      },
+      type: 'Feature',
+    },
+    {
+      geometry: {
+        coordinates: [
+          -0.347987732253237,
+          51.4324653882346,
+        ],
+        type: 'Point',
+      },
+      properties: {
+        category: 'Retail',
+        name: 'Aesop',
+      },
+      type: 'Feature',
+    },
+  ],
+};
+
+const metaMarkersB = {
+  id: '2',
+  label: 'B',
+  name: 'A.P.C',
+  type: 'FeatureCollection',
+  features: [
+    {
+      geometry: {
+        coordinates: [
+          -0.347981788742138,
+          51.4323681173447,
+        ],
+        type: 'Point',
+      },
+      properties: {
+        category: 'Retail',
+        name: 'A.P.C',
+      },
+      type: 'Feature',
+    },
+    {
+      geometry: {
+        coordinates: [
+          -0.348029081486836,
+          51.4325423397993,
+        ],
+        type: 'Point',
+      },
+      properties: {
+        category: 'Retail',
+        name: 'A.P.C',
+      },
+      type: 'Feature',
+    },
+    {
+      geometry: {
+        coordinates: [
+          -0.34796932686546,
+          51.4323630758005,
+        ],
+        type: 'Point',
+      },
+      properties: {
+        category: 'Retail',
+        name: 'A.P.C',
+      },
+      type: 'Feature',
+    },
+  ],
+};
+
+const metaMarkersC = {
+  id: '3',
+  label: 'C',
+  name: 'Steves Magical House',
+  type: 'FeatureCollection',
+  features: [
+    {
+      geometry: {
+        coordinates: [
+          -0.35156215809263,
+          51.4328170652326,
+        ],
+        type: 'Point',
+      },
+      properties: {
+        category: 'Magic',
+        name: 'Steves Magical House',
+      },
+      type: 'Feature',
+    },
+  ],
+};
+
+export { metaMarkersA, metaMarkersB, metaMarkersC };
