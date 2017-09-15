057c321938912306e34f35e645b9a80fffa67288
Faraz Khan <farazcsk@gmail.com>
2017-09-15 10:15:35 +0100

    Add brand icons

--- styleguide/screens/Design/Iconography.js
+++ styleguide/screens/Design/Iconography.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import IconShowcase from '../../components/IconShowcase/IconShowcase';
 
-import { uiIcons, brandIcons } from './icons';
+import { uiIcons, brandIcons, valueIcons } from './icons';
 
 import type from '../../typography.css';
 import css from './Iconography.css';
@@ -24,6 +24,17 @@ const Iconography = () => (
     <h2 className={ type.h2 }>Brand Icons</h2>
     <div className={ css.iconGroup }>
       { brandIcons.map(icon => (
+        <IconShowcase
+          key={ icon.value }
+          className={ css.icon }
+          name={ icon.name }
+          value={ icon.value }
+        />
+      )) }
+    </div>
+    <h2 className={ type.h2 }>Value Icons</h2>
+    <div className={ css.iconGroup }>
+      { valueIcons.map(icon => (
         <IconShowcase
           key={ icon.value }
           className={ css.icon }
