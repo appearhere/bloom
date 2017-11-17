695c5566547fad3928711545367b4445d99b7289
Faraz Khan <farazcsk@gmail.com>
2017-11-17 16:21:17 +0000

    Upgrade react-on-visible

--- components/Animate/Sunrise.test.js
+++ components/Animate/Sunrise.test.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import Sunrise from './Sunrise';
 
+jest.mock('react-on-visible');
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Sunrise />, div);
