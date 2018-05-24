858f5c267a7ea50ad84491df1902d9581966e1c0
Gregorio Setti <gregorio.setti@gmail.com>
2018-05-24 11:06:44 +0100

    Remove fallback from IconHelper

--- packages/core/src/components/Icon/iconHelper.js
+++ packages/core/src/components/Icon/iconHelper.js
@@ -1,42 +1,21 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import classnames from 'classnames';
-import warning from 'warning';
 import css from './IconCore.css';
 
 export default (iconSet, theme) => class Icon extends Component {
   static propTypes = {
     name: PropTypes.string.isRequired,
-    fallback: PropTypes.string,
     className: PropTypes.string,
   };
 
-  constructor(props) {
-    super(props);
-
-    warning(
-      props.fallback || iconSet[props.name],
-      `Icon(): No icon exists for ${props.name}`
-    );
-
-    this.createMarkup = this.createMarkup.bind(this);
-  }
-
-  createMarkup() {
-    const { name, fallback } = this.props;
-
-    return { __html: iconSet[name] || fallback };
-  }
-
   render() {
-    const { className, name, fallback, ...rest } = this.props;
-    const fallingBack = !iconSet[name] && fallback;
+    const { className, name, ...rest } = this.props;
     const IconComponent = iconSet[name];
     const classes = classnames(
       css.root,
       theme ? theme.root : null,
       theme ? theme[name] : css[name],
-      theme && fallingBack ? theme.fallback : null,
       className,
     );
 
