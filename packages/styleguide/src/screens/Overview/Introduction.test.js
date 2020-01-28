23dab462441d5ebc833d75a199f2f1a4d192cc2e
dbatten5 <dom.batten@appearhere.co.uk>
2020-01-28 08:53:29 +0000

    Revert "CHORE Allow markdown component to open links in new tabs"

--- packages/styleguide/src/screens/Overview/Introduction.test.js
+++ packages/styleguide/src/screens/Overview/Introduction.test.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 
 import Introduction from './Introduction';
 
-it.skip('renders without crashing', () => {
+it('renders without crashing', () => {
   const div = document.createElement('div');
   render(<Introduction />, div);
 });
