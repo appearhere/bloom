d67c391665e7b78206a9415fa014f0c375ff0aa5
Martin Denyer <mtdenyer@gmail.com>
2020-06-27 14:31:30 +0100

    Add icon knob for the IconInput stories

--- packages/playground/stories/IconInput.story.js
+++ packages/playground/stories/IconInput.story.js
@@ -8,11 +8,65 @@ const stories = storiesOf('FormComponents', module);
 stories.addDecorator(withKnobs);
 
 const inputTypes = ['text', 'email', 'password', 'search', 'url', 'textarea'];
+const icons = [
+  'appearhere-brackets',
+  'automatic-payments',
+  'calendar-insight',
+  'chart-arrow',
+  'play-c',
+  'chevron-right',
+  'tick-c',
+  'tick-starred',
+  'travel-idea',
+  'vip-entrance',
+  'card-list',
+  'account',
+  'appearhere',
+  'arrow',
+  'back',
+  'arrow-right',
+  'bogroll',
+  'book',
+  'calendar',
+  'camera',
+  'chatting',
+  'chevron',
+  'clock',
+  'comment',
+  'cross',
+  'dollar',
+  'download',
+  'facebook',
+  'filter',
+  'globe',
+  'heart',
+  'location',
+  'manage',
+  'map',
+  'menu',
+  'minus',
+  'notification',
+  'percentage',
+  'pintrest',
+  'play',
+  'plus',
+  'radio',
+  'search',
+  'shield',
+  'signature',
+  'star',
+  'store',
+  'teamwork',
+  'tick',
+  'ticket',
+  'twitter',
+  'instagram',
+];
 
 stories
   .add('IconInput', () => (
     <IconInput
-      iconName="search"
+      iconName={select('Icon', icons, 'search')}
       id="1"
       type={select('Type', inputTypes, inputTypes[0])}
       value="100"
@@ -24,7 +78,7 @@ stories
   ))
   .add('IconInput w/ rightsided icon', () => (
     <IconInput
-      iconName="search"
+      iconName={select('Icon', icons, 'search')}
       iconSide="right"
       id="1"
       type={select('Type', inputTypes, inputTypes[0])}
@@ -37,7 +91,7 @@ stories
   ))
   .add('IconInput w/ high priority', () => (
     <IconInput
-      iconName="search"
+      iconName={select('Icon', icons, 'search')}
       id="1"
       type={select('Type', inputTypes, inputTypes[0])}
       value="100"
@@ -50,7 +104,7 @@ stories
   ))
   .add('IconInput w/ custom icon dimensions', () => (
     <IconInput
-      iconName="search"
+      iconName={select('Icon', icons, 'search')}
       iconDimensions={{ height: 32, width: 32 }}
       id="1"
       type={select('Type', inputTypes, inputTypes[0])}
