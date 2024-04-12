c1f49b797c97c359eadfee6f209c5fe0da12083e
Luke Donovan <lukelamnisos@icloud.com>
2024-04-12 10:23:39 +0100

    Fix bloom pipeline - update node and jest - fix failing tests - fix storybook

--- packages/core/src/components/Cards/DestinationListingCard/DestinationListingCard.test.js
+++ packages/core/src/components/Cards/DestinationListingCard/DestinationListingCard.test.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+global.MutationObserver = class {
+  constructor(callback) {}
+  disconnect() {}
+  observe(element, initObject) {}
+};
+
+jest.mock('nuka-carousel', () => {
+  const React = require('react');
+  return jest.fn(() => <div>Mocked Carousel</div>);
+});
+
 import DestinationListingCard from './DestinationListingCard';
 
 it('renders without crashing', () => {
